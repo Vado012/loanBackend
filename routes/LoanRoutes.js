37591766103c86df082cfc32c60406665113599d
Vado012 <okamkpachinedu@gmail.com>
2026-02-25 16:11:05 +0100

    work

--- routes/LoanRoutes.js
+++ routes/LoanRoutes.js
@@ -4,7 +4,7 @@ import authorize from '../middleware/Authorization.js';
 const router = e.Router();
 
 router.post('/',authorize(["admin","user"]), applyLoan)
-router.get('/all',authorize(["admin"]), getAllLoan)
+router.get('/all',authorize(["admin","user"]), getAllLoan)
 router.get('/myloans',authorize(["user"]), getUserLoans)
 router.delete('/:id',authorize(["admin", "user"]), deleteLoan)
 router.put('/:id', updateLoan)
